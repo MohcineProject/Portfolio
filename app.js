c33851c9b6d738cbb0bdfa2be6ca50b69f09bd48
MohcinProject <ZahdiMohcine20@gmail.com>
2024-02-11 22:08:03 +0100

    add sections passing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section') ; 
 const sectBtns = document.querySelectorAll('.controls') ;
 const sectBtn = document.querySelectorAll('.control') ; 
-const allSections = document.querySelectorAll('.main-content') ; 
+const allSections = document.querySelector('.main-content') ; 
 
 function PageTransition(){
     //Button click active class
@@ -16,8 +16,24 @@ function PageTransition(){
 
     //Sections active class
     allSections.addEventListener('click',(e)=>{
-        console.log(e.target) ; 
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the othes btn
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active') ; 
+            })
+            e.target.classList.add('active');
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+            
+        }
     })
 }
 
 PageTransition() ; 
+
